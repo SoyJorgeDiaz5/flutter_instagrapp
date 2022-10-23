1b7ea49ec4e263e5e53b782128c2367c6f6fa8a1
SoyJorgeDiaz5 <soyjorgediaz5@gmail.com>
2022-10-22 23:41:41 -0500

    feat(story): adds stories fake data source

---
+++ lib/data/data.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_instagrapp/models/story_model.dart';
+
+final stories = [
+  StoryModel(
+      'https://images.pexels.com/photos/3417784/pexels-photo-3417784.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'Alex'),
+  StoryModel(
+      'https://images.pexels.com/photos/12470935/pexels-photo-12470935.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'Bea'),
+  StoryModel(
+      'https://images.pexels.com/photos/3660548/pexels-photo-3660548.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'Carl'),
+  StoryModel(
+      'https://images.pexels.com/photos/12470921/pexels-photo-12470921.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'David'),
+  StoryModel(
+      'https://images.pexels.com/photos/2884864/pexels-photo-2884864.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'Eric'),
+  StoryModel(
+      'https://images.pexels.com/photos/14008004/pexels-photo-14008004.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'Fred'),
+  StoryModel(
+      'https://images.pexels.com/photos/3612089/pexels-photo-3612089.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'Gina'),
+];

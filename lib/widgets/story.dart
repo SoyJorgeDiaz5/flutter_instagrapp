ba5933a273a179d4ca289afe40dd9e48b58818df
SoyJorgeDiaz5 <soyjorgediaz5@gmail.com>
2022-10-22 23:41:52 -0500

    feat(story): adds Story and CircleColor widgets

---
+++ lib/widgets/story.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class Story extends StatelessWidget {
+  final String image;
+  final String name;
+
+  const Story({
+    Key? key,
+    required this.image,
+    required this.name,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          alignment: Alignment.center,
+          children: [
+            const CircleColor(),
+            Container(
+              width: 80,
+              height: 80,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+              ),
+            ),
+            CircleAvatar(
+              radius: 35,
+              backgroundImage: NetworkImage(
+                image,
+              ),
+            )
+          ],
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Text(name)
+      ],
+    );
+  }
+}
+
+class CircleColor extends StatelessWidget {
+  const CircleColor({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 90,
+      height: 90,
+      decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color(0xffee0000),
+                Color(0xffeeee00),
+              ])),
+    );
+  }
+}

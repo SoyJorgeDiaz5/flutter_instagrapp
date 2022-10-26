20fd2099251a3b896173e656c6924cded6338324
SoyJorgeDiaz5 <soyjorgediaz5@gmail.com>
2022-10-25 23:38:04 -0500

    feat(post): adds PostItem widget

--- lib/main.dart
+++ lib/main.dart
@@ -33,13 +33,97 @@ class InstagrappHome extends StatelessWidget {
         title: const Text('Instagrapp'),
       ),
       body: ListView(
-        scrollDirection: Axis.horizontal,
-        children: stories
-            .map((story) => Story(
-                  image: story.image,
-                  name: story.name,
-                ))
-            .toList(),
+        children: [
+          SingleChildScrollView(
+            // padding: const EdgeInsets.only(left: 4.0, right: 4.0),
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: stories
+                  .map((story) => Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Story(
+                          image: story.image,
+                          name: story.name,
+                        ),
+                  ))
+                  .toList(),
+            ),
+          ),
+          const PostItem(),
+          const PostItem(),
+          const PostItem(),
+          const PostItem(),
+          const PostItem(),
+          const PostItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class PostItem extends StatelessWidget {
+  const PostItem({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 400,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: const [
+                    CircleAvatar(
+                      radius: 16,
+                      backgroundImage: NetworkImage(
+                        'https://images.pexels.com/photos/4923041/pexels-photo-4923041.jpeg?auto=compress&cs=tinysrgb&w=600',
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    Text('Howard'),
+                  ],
+                ),
+                const Icon(Icons.more_vert)
+              ],
+            ),
+          ),
+          const Image(
+            height: 300,
+            fit: BoxFit.cover,
+            image: NetworkImage(
+              'https://images.pexels.com/photos/10756534/pexels-photo-10756534.jpeg?auto=compress&cs=tinysrgb&w=600',
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: const [
+                    Icon(Icons.favorite_border),
+                    Padding(
+                      padding: EdgeInsets.only(left: 8.0),
+                      child: Icon(Icons.comment),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 8.0),
+                      child: Icon(Icons.share),
+                    ),
+                  ],
+                ),
+                const Icon(Icons.bookmark_add_outlined),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
